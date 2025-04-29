164bb5455161455401219b9013e67a8bea62eafc
Valentinaadle <valentinaadle1@gmail.com>
2025-04-29 19:15:39 -0300

    agregue app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+// Configuración de la carpeta de vistas y el motor EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Configuración de carpeta pública
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.get('/', (req, res) => {
+  res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Levantar el servidor
+app.listen(PORT, () => {
+  console.log('Servidor funcionando en http://localhost:${PORT}');
+});
